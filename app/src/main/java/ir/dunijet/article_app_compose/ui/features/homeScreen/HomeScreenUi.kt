e98527eea21c9682074ab1ccf79fe07a99d4b519
Amir Hossein Mohammadi <amir00462@gmail,com>
2023-03-24 14:48:06 +0330

    paging - bug fixed

--- app/src/main/java/ir/dunijet/article_app_compose/ui/features/homeScreen/HomeScreenUi.kt
+++ app/src/main/java/ir/dunijet/article_app_compose/ui/features/homeScreen/HomeScreenUi.kt
@@ -1,30 +1,20 @@
 package ir.dunijet.article_app_compose.ui.features.homeScreen
 
 import android.app.Activity
-import android.util.Log
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
-import androidx.paging.PagingData
-import androidx.paging.compose.collectAsLazyPagingItems
-import androidx.paging.map
 import dev.burnoo.cokoin.navigation.getNavViewModel
-import ir.dunijet.article_app_compose.data.model.Article
 import ir.dunijet.article_app_compose.ui.theme.cBackground
 import ir.dunijet.article_app_compose.ui.widgets.HomeContent
 import ir.dunijet.article_app_compose.ui.widgets.HomeDrawer
 import ir.dunijet.article_app_compose.ui.widgets.HomeToolbar
-import ir.dunijet.article_app_compose.util.listArticle
-import kotlinx.coroutines.flow.collectIndexed
-import kotlinx.coroutines.flow.collectLatest
-import kotlinx.coroutines.flow.flowOf
 import kotlinx.coroutines.launch
-import org.koin.core.parameter.parametersOf
+import androidx.paging.compose.collectAsLazyPagingItems
 
 @Composable
 fun HomeScreenUi() {
@@ -35,20 +25,7 @@ fun HomeScreenUi() {
     val scope = rememberCoroutineScope()
 
     val articles = viewModel.getArticles()
-    val lazyPagingItems = flowOf(PagingData.from(listArticle)).collectAsLazyPagingItems()
-
-    LaunchedEffect(null) {
-
-        Log.v("testArticle", "hello")
-
-        scope.launch {
-            articles.collectIndexed { index, value ->
-
-                Log.v("testArticle", index.toString() + "  -  " + value.toString())
-
-            }
-        }
-    }
+    val lazyPagingItems = articles.collectAsLazyPagingItems()
 
     Scaffold(
         scaffoldState = scaffoldState,
@@ -79,7 +56,7 @@ fun HomeScreenUi() {
         drawerBackgroundColor = cBackground
     ) {
 
-        HomeContent(null)
+        HomeContent(lazyPagingItems)
 
     }
 
